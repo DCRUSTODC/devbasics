36319893db87da044531b6cbba045279188605ef
srish-03 <85109575+srish-03@users.noreply.github.com>
2023-08-22 17:03:14 +0530

    Add files via upload

---
+++ JS-music-player/script.js
@@ -0,0 +1,73 @@
+const musicContainer = document.getElementById("music-container");
+const playBtn = document.getElementById("play");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+const audio = document.getElementById("audio");
+const progress = document.getElementById("progress");
+const progressContainer = document.getElementById("progress-container");
+const title = document.getElementById("title");
+const cover = document.getElementById("cover");
+
+const songs = ["Bas Mein"];
+let songIndex = 0;
+loadSong(songs[songIndex]);
+function loadSong(song) {
+    title.innerText = song;
+    audio.src = `music.mp3`;
+    cover.src = `images.jpg`;
+}
+function playSong() {
+    musicContainer.classList.add("play");
+    playBtn.querySelector("i.fa").classList.remove("fa-play");
+    playBtn.querySelector("i.fa").classList.add("fa-pause");
+    audio.play();
+}
+function pauseSong() {
+    musicContainer.classList.remove("play");
+    playBtn.querySelector("i.fa").classList.add("fa-play");
+    playBtn.querySelector("i.fa").classList.remove("fa-pause");
+    audio.pause();
+}
+
+function prevSong() {
+    songIndex--;
+    if (songIndex < 0) {
+        songIndex = songs.length - 1;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+function nextSong() {
+    songIndex++;
+    if (songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+function updateProgress(e) {
+    const { duration, currentTime } = e.srcElement;
+    const progressPerCent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPerCent}%`;
+}
+function setProgress(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+    audio.currentTime = (clickX / width) * duration;
+}
+
+playBtn.addEventListener("click", () => {
+    const isPlaying = musicContainer.classList.contains("play");
+    if (isPlaying) {
+        pauseSong();
+    } else {
+        playSong();
+    }
+});
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
+audio.addEventListener("timeupdate", updateProgress);
+progressContainer.addEventListener("click", setProgress);
+
+audio.addEventListener("ended", nextSong);
